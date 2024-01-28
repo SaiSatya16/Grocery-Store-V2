20dc23224113e3251ba8a8d8f4d230a0049c4a11
jj1039 <jj1039@srmist.edu.in>
2024-01-28 23:34:11 +0530

    Update login.js

--- static/vue/components/login.js
+++ static/vue/components/login.js
@@ -21,7 +21,55 @@ const Login =Vue.component('login', {
           <p class="mt-3">Don't have an account? <router-link to="/Manager_Registration">Register as Manager here</router-link></p>
         
       </div>
+
+      <div>
+
+      <h2> About the project</h2>
+      <h5>This project is a sophisticated multi-user grocery store web application that seamlessly integrates Vue.js for the frontend and Flask for the backend. Bootstrap ensures an intuitive, user-friendly interface, allowing for dynamic category and product management. The application also features a robust authentication system with role-based access control, allowing for three different user role types: admin, manager, and customer. Admins can manage categories and approve manager requests. Managers can manage products within approved categories. Customers can shop and place orders.</h5>
+      <h2>Usage</h2>
+
+<h5>Login as an admin to manage categories and approve manager requests. Login as a manager to manage products within approved categories. Login as a customer to shop and place orders.</h5>
+
+<h4>Admin Credentials:</h4>
+<ul>
+    <li><strong>Email:</strong> admin@gmail.com</li>
+    <li><strong>Password:</strong> admin</li>
+</ul>
+
+<h4>Manager Credentials:</h4>
+<ul>
+    <li><strong>Email:</strong> manager@gmail.com</li>
+    <li><strong>Password:</strong> manager</li>
+</ul>
+
+<h4>Customer Credentials:</h4>
+<ul>
+    <li><strong>Email:</strong> customer@gmail.com</li>
+    <li><strong>Password:</strong> customer</li>
+</ul>
+
+<h5>You can also create your own account and login.</h5>
+<h5>You can register as a manager and wait for admin approval.</h5>
+<h5>You can register as a customer and start shopping.</h5>
+
+<div>
+<h2> Source Code</h2>
+<h5>Source code for this project is available on GitHub.</h5>
+<a href="https://github.com/SaiSatya16/Grocery-Store-V2"> Click here to view the source code</a>
+
+<h5> Note: I also implemented backround jobs such as downloading product details, sending daily remainders to customers, sending monthely reports to managers as emails. But I have not included them in deployment. If you want to see them, please visit the source code on GitHub and follow the steps to run project locally.</h5>
+
+</div>
+
+
+
+
+
+      </div>
+
+
     </div>
+    
   </div>`,
   data() {
     return {
